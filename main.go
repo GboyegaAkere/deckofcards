cc98653d945fde19c0cdc13345e16995afe21064
GboyegaAkere <oluwagboyegaakeremale80@gmail.com>
2024-01-11 16:40:12 +0100

    variables in go

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	var cards string = "Ace of Spaces"
+	// var cards string = "Ace of Spades"
+	cards := "Ace of Spades"
+	cards = "five of Diamonds"
 	fmt.Println(cards)
 }
