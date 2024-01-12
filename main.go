dc037012180d41d053a18322fb2de4451b095113
GboyegaAkere <oluwagboyegaakeremale80@gmail.com>
2024-01-12 08:52:41 +0100

    adding another function

--- main.go
+++ main.go
@@ -2,9 +2,18 @@ package main
 
 import "fmt"
 
+// func main() {
+// 	// var cards string = "Ace of Spades"
+// 	cards := "Ace of Spades"
+// 	cards = "five of Diamonds"
+// 	fmt.Println(cards)
+// }
+
 func main() {
-	// var cards string = "Ace of Spades"
-	cards := "Ace of Spades"
-	cards = "five of Diamonds"
-	fmt.Println(cards)
+	card := newCard()
+	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Ace of Spades"
 }
